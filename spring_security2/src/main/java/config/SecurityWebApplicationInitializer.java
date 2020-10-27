b81bd1d9da21bab0efe44d265d4eb0b116d60728
wujun728 <wujun728@hotmail.com>
2020-10-27 08:08:11 +0800

    1

--- spring_security2/src/main/java/config/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
